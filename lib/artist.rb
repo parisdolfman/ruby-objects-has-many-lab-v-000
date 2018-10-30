a8ad0d056c5c7c9d7f5b0651d2a71ffb8128442c
Paris Dolfman <parisdolfman@gmail.com>
2018-10-30 19:16:16 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,7 @@
 class Artist
- attr_accessor :name, :songs, :artist_name
+ attr_accessor :name, :songs, :artist_name, :song_count
+
+@@song_count = 0
 
  def initialize(name) #takes in argument of name upon initialization
    @name = name
@@ -10,14 +12,19 @@ class Artist
  def add_song(song)
    @songs << song #shovels song provided in argument onto empty array
    song.artist = self #self refers to artist on which we are calling add_song. #.artist is being called on song being passed in as agument and sets to self = the artist
+   @@song_count += 1
  end
 
  def add_song_by_name(new_name)
   song = Song.new(new_name) #sets variable song to equal a new instantiation of obect Song and adds argument of new_name to set the new name
    @songs << song  #shovels new song object onto empty array
    song.artist = self #self refers to artist on which we are calling add_song_by_name. #.artist is being called on the song being passed in as agument and sets to self = the artist
+   @@song_count += 1
  end
 
+def self.song_count
+  @@song_count
+end
 
 
 
