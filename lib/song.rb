cedb480dda126b85330b6f33773d3fcf6833ba8c
Paris Dolfman <parisdolfman@gmail.com>
2018-10-31 16:08:01 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,21 +5,21 @@ class Song
 @@all = [] #empty array to equal variable
 
  def initialize(name) #when object instantiated, whatever argument passed assigned variable 'name'
-   @name = name #attribute writer, returns instance of variable
-   @artist = artist
+   @name = name #sets argument to equal instance variable we can work with
+   @artist = artist #same as above
    @@all << self
  end
 
-def artist_name
- if self.artist
-   self.artist.name
+def artist_name #object knows the name of it's artist
+ if self.artist #self points to current object, this gives the artist of the object
+   self.artist.name #returns name of the artist of the object
  else
-   nil
+   nil #returns nil if the song does not have an artist
    end
 end
 
-def self.all
- @@all
+def self.all #explicit receiver of .all is class Song
+ @@all #returns array that all objects have been shoveled into above
 end
 
 end
