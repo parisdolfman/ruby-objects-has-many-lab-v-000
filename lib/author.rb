3ddb01a5013b70af6e5e15cfa5eb9217f4eb840e
Paris Dolfman <parisdolfman@gmail.com>
2018-10-31 19:47:08 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,7 @@
 class Author
 attr_accessor :name, :posts
 
+@@post_count = 0
 
 def initialize(name)
   @name = name
@@ -10,12 +11,18 @@ end
 def add_post(post)
   post.author = self
   @posts << post
+  @@post_count += 1
 end
 
 def add_post_by_title(title)
   post = Post.new(title)
   post.author = self
+  @posts << post
+  @@post_count += 1
+end
 
+def self.post_count
+  @@post_count
 end
 
 end
